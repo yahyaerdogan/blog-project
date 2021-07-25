70746e2d86f1262bd836dc59518169dc2feac591
yahyaerdogan <yahyaerdogan1199@gmail.com>
2021-07-26 01:01:11 +0300

    Pillow package and media settings

--- blog/models.py
+++ blog/models.py
@@ -11,5 +11,6 @@ class Post(models.Model):
         on_delete=models.CASCADE
         )
     date= models.DateTimeField(auto_now_add=True)
+    image=models.ImageField(upload_to="images/", null=True)
     def __str__(self):
         return self.title
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
     path("about/", TemplateView.as_view(template_name="about.html")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
